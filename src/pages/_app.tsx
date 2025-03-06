02fc66ed0b39309f3fa363364a123c23315174e3
kwmr0605 <kotaro.yuto.k@gmail.com>
2025-03-06 09:18:02 +0900

    feature: css周りの設定変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '@/styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
