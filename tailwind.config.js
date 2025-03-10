a05b5202d20ac1d85bf6abc5aa83f8be4825d00b
kwmr0605 <kotaro.yuto.k@gmail.com>
2025-03-10 09:47:51 +0900

    style: Add custom color palette to Tailwind config and update global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        baseColor: "#d8e6e0",
+        accentColor: "#4a9283",
+        fontColor: "#2c3e3b",
+      },
+    },
   },
   plugins: [],
-}
-
+};
