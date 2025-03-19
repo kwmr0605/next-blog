8c30027438399ad3e40e3b0d33bb0a5911aea0cd
kwmr0605 <kotaro.yuto.k@gmail.com>
2025-03-19 12:14:01 +0900

    fix: Update table of contents rendering to use jQuery for text extraction

--- src/libs/render_toc.ts
+++ src/libs/render_toc.ts
@@ -4,7 +4,7 @@ export const renderToc = (body: string) => {
   const $ = cheerio.load(body);
   const headings = $("h1, h2, h3").toArray();
   const toc = headings.map((data) => ({
-    text: data.children[0].data,
+    text: $(data).text(),
     id: data.attribs.id,
     name: data.name,
   }));
